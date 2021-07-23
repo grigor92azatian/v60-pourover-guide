a317626cc7e5c59541d7dd43d95b9c743aaa0006
Grigor Azatian <grigorazatian@yahoo.com>
2021-07-22 18:48:10 -0700

    started work on the expandIcon function to be used in conjunction with eventListener for the brew method icons

---
+++ script.js
@@ -0,0 +1,6 @@
+//create function for event hover that will be added to all of the 6 brew method icons. When the mouse hovers over the icon, the icon will expand to a bigger circle and give a bit more detail
+//about brew method in question
+
+function expandIcon(){
+    
+}
